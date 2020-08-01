29cbb1392d1bc882998168d63400d1c07dac06c4
dan13ram <dan13ram@gmail.com>
2020-08-01 15:53:59 +0530

    handleBuy flow done

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
     Contract,
     Buy,
@@ -21,15 +21,16 @@ import { FileEntity, UserEntity } from "../generated/schema";
 export function handleSell(event: Sell): void {
     let contract = Contract.bind(event.address);
     let fileId = event.params.fileId;
+    let seller = event.params.seller;
     let fileObject = contract.Files(fileId);
 
     let file = new FileEntity(fileId.toHex());
     file.metadataHash = fileObject.value2;
 
-    let user = UserEntity.load(fileObject.value0.toHex());
+    let user = UserEntity.load(seller.toHex());
     if (user == null) {
-        user = new UserEntity(fileObject.value0.toHex());
-        user.address = fileObject.value0;
+        user = new UserEntity(seller.toHex());
+        user.address = seller;
         user.filesBought = new Array<string>();
         user.filesOwned = new Array<string>();
         user.subscribers = new Array<string>();
@@ -44,75 +45,38 @@ export function handleSell(event: Sell): void {
     file.price = fileObject.value3;
     file.priceAsset = fileObject.value1;
     file.buyers = new Array<string>();
+    file.numBuys = BigInt.fromI32(0);
     file.save();
 }
 
-export function handleBuy(event: Buy): void {}
-/*   // Entities can be loaded from the store using a string ID; this ID */
-/*   // needs to be unique across all entities of the same type */
-/*   let entity = ExampleEntity.load(event.transaction.from.toHex()) */
+export function handleBuy(event: Buy): void {
+    let contract = Contract.bind(event.address);
+    let fileId = event.params.fileId;
+    let buyer = event.params.buyer;
+    let fileObject = contract.Files(fileId);
 
-/*   // Entities only exist after they have been saved to the store; */
-/*   // `null` checks allow to create entities on demand */
-/*   if (entity == null) { */
-/*     entity = new ExampleEntity(event.transaction.from.toHex()) */
+    let file = FileEntity.load(fileId.toHex());
 
-/*     // Entity fields can be set using simple assignments */
-/*     entity.count = BigInt.fromI32(0) */
-/*   } */
+    let user = UserEntity.load(buyer.toHex());
+    if (user == null) {
+        user = new UserEntity(buyer.toHex());
+        user.address = buyer;
+        user.filesBought = new Array<string>();
+        user.filesOwned = new Array<string>();
+        user.subscribers = new Array<string>();
+        user.subscriptions = new Array<string>();
+    }
+    let boughtFiles = user.filesBought;
+    boughtFiles.push(file.id);
+    user.filesBought = boughtFiles;
+    user.save();
 
-/*   // BigInt and BigDecimal math are supported */
-/*   entity.count = entity.count + BigInt.fromI32(1) */
-
-/*   // Entity fields can be set based on event parameters */
-/*   entity.fileId = event.params.fileId */
-/*   entity.buyer = event.params.buyer */
-
-/*   // Entities can be written to the store with `.save()` */
-/*   entity.save() */
-
-/*   // Note: If a handler doesn't require existing field values, it is faster */
-/*   // _not_ to load the entity from the store. Instead, create it fresh with */
-/*   // `new Entity(...)`, set the fields that should be updated and save the */
-/*   // entity back to the store. Fields that were not set or unset remain */
-/*   // unchanged, allowing for partial updates to be applied. */
-
-/*   // It is also possible to access smart contracts from mappings. For */
-/*   // example, the contract that has emitted the event can be connected to */
-/*   // with: */
-/*   // */
-/*   // let contract = Contract.bind(event.address) */
-/*   // */
-/*   // The following functions can then be called on this contract to access */
-/*   // state variables and other data: */
-/*   // */
-/*   // - contract.getEarnings(...) */
-/*   // - contract.nextStreamId(...) */
-/*   // - contract.getCompoundingStream(...) */
-/*   // - contract.balanceOf(...) */
-/*   // - contract.fileCount(...) */
-/*   // - contract.isPauser(...) */
-/*   // - contract.Files(...) */
-/*   // - contract.buyerInfo(...) */
-/*   // - contract.paused(...) */
-/*   // - contract.sellerToBuyer(...) */
-/*   // - contract.sell(...) */
-/*   // - contract.cancelStream(...) */
-/*   // - contract.withdrawFromStream(...) */
-/*   // - contract.createCompoundingStream(...) */
-/*   // - contract.getStream(...) */
-/*   // - contract.interestOf(...) */
-/*   // - contract.owner(...) */
-/*   // - contract.isOwner(...) */
-/*   // - contract.buyerToSeller(...) */
-/*   // - contract.isCompoundingStream(...) */
-/*   // - contract.deltaOf(...) */
-/*   // - contract.createStream(...) */
-/*   // - contract.cTokenManager(...) */
-/*   // - contract.buy(...) */
-/*   // - contract.fee(...) */
-/*   // - contract.priceLimit(...) */
-/* } */
+    let buyers = file.buyers;
+    buyers.push(user.id);
+    file.buyers = buyers;
+    file.numBuys = file.numBuys + BigInt.fromI32(1);
+    file.save();
+}
 
 export function handleCreateCompoundingStream(
     event: CreateCompoundingStream
